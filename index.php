7661aba9088b392c2608f551fece1904d238f0cb
serratiago <contato@tiagoserra.com>
2017-06-08 23:42:29 -0300

    Retorno de conexão
    Quando conecta

--- index.php
+++ index.php
@@ -63,6 +63,7 @@ include("php/conexao.php");
 try {
     $conn = new PDO("sqlsrv:server = tcp:dbservers2m.database.windows.net,1433; Database = dbwebS2M", "dbservers2m", "X28t12r80s");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     print("Ok!");
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
