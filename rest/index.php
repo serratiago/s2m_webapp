e2c45b937109a8b075351a79e2fbbe0fc4d0e455
serratiago <contato@tiagoserra.com>
2017-06-19 01:35:29 -0300

    conexao
    select

--- rest/index.php
+++ rest/index.php
@@ -9,10 +9,14 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "dbservers2m@dbservers2m", "pwd" => "X28t12r80s", "Database" => "dbwebS2M", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:dbservers2m.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+$tsql= "select int_id_convenio, str_nome_convenio from tbl_convenio";
+$getResults= sqlsrv_query($conn, $tsql);
+
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    echo ($row['int_id_convenio'] . " - " . $row['str_nome_convenio'] . PHP_EOL);
+}
+sqlsrv_free_stmt($getResults);
 
 
 exit();
