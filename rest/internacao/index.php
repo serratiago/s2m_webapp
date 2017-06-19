a30e153f53659f58e1efbe14b569e8e567e696e6
serratiago <contato@tiagoserra.com>
2017-06-19 12:15:12 -0300

    correção
    erro de coluna

--- rest/internacao/index.php
+++ rest/internacao/index.php
@@ -23,12 +23,12 @@ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
 	$int_sexo_paciente = utf8_encode($row['int_sexo_paciente']);
 	$int_dt_nascimento_paciente = utf8_encode($row['int_dt_nascimento_paciente']);
 	$str_nome_medico = utf8_encode($row['str_nome_medico']);
-	$int_id_andar_quarto = utf8_encode($row['int_id_andar_quarto ']);
+	$int_num_andar_quarto = utf8_encode($row['int_num_andar_quarto ']);
 	$int_num_quarto = utf8_encode($row['int_num_quarto']);
 
 	$json .= '{"str_nome_paciente":"'.$str_nome_paciente.'","str_nome_convenio":"'.$str_nome_convenio.'",'; 
 	$json .= '"int_sexo_paciente":"'.$int_sexo_paciente.'","int_dt_nascimento_paciente":"'.$int_dt_nascimento_paciente.'",'; 
-	$json .= '"str_nome_medico":"'.$str_nome_medico.'","int_id_andar_quarto":"'.$int_id_andar_quarto.'",'; 
+	$json .= '"str_nome_medico":"'.$str_nome_medico.'","int_id_andar_quarto":"'.$int_num_andar_quarto.'",'; 
 	$json .= '"int_num_quarto":"'.$int_num_quarto.'"},'; 
 	
 }
