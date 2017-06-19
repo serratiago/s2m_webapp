a10e378b45587b12eab6b9c8e142e62ff0a5c373
serratiago <contato@tiagoserra.com>
2017-06-19 00:50:25 -0300

    Acesso ao banco
    lendo tabela

--- rest/index.php
+++ rest/index.php
@@ -16,16 +16,13 @@ $connectionOptions = array(
 );
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
-$tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
-     FROM [SalesLT].[ProductCategory] pc
-     JOIN [SalesLT].[Product] p
-     ON pc.productcategoryid = p.productcategoryid";
+$tsql= "select int_id_convenio, str_nome_convenio from tbl_convenio";
 $getResults= sqlsrv_query($conn, $tsql);
 echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
     echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    echo ($row['int_id_convenio'] . " " . $row['str_nome_convenio'] . PHP_EOL);
 }
 sqlsrv_free_stmt($getResults);
 
