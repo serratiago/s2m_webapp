444df5263d88cb8983bbee29ac7ddb09bc9650fd
serratiago <contato@tiagoserra.com>
2017-06-19 02:06:31 -0300

    teste
    rest

--- rest/index.php
+++ rest/index.php
@@ -6,7 +6,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$request = explode('-', trim($_SERVER['PATH_INFO'],'-'));
 $input = json_decode(file_get_contents('php://input'),true);
  
 // retrieve the table and key from the path
