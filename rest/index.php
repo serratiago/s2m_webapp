a94e688c983b2c9b45f2ae5250fd237932b40144
serratiago <contato@tiagoserra.com>
2017-06-19 01:06:20 -0300

    Banco
    Lendo tabela

--- rest/index.php
+++ rest/index.php
@@ -18,11 +18,13 @@ $connectionOptions = array(
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 $tsql= "select int_id_convenio, str_nome_convenio from tbl_convenio";
 $getResults= sqlsrv_query($conn, $tsql);
-
+echo ("<br>Reading data from table: " . PHP_EOL);
+echo ("<br>Resultado: ". $getResults);
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
     echo ($row['int_id_convenio'] . " " . $row['str_nome_convenio'] . PHP_EOL);
 }
-
 sqlsrv_free_stmt($getResults);
 
 
