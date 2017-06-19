c5348c3d7dc03dcd4fa07f1e0ea347c336d9efd7
serratiago <contato@tiagoserra.com>
2017-06-19 01:43:56 -0300

    versão final
    rest

--- rest/index.php
+++ rest/index.php
@@ -1,32 +1,9 @@
 <?php
-echo ("Teste!");
-// Dados do banco
-$dbhost   = "dbservers2m.database.windows.net";   #Nome do host
-$db       = "dbwebS2M";   #Nome do banco de dados
-$user     = "dbservers2m"; #Nome do usuário
-$password = "X28t12r80s";
-
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "dbservers2m@dbservers2m", "pwd" => "X28t12r80s", "Database" => "dbwebS2M", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:dbservers2m.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-
-$tsql= "select int_id_convenio, str_nome_convenio from tbl_convenio";
-$getResults= sqlsrv_query($conn, $tsql);
-
-
-if( $conn === false ) {
-     die( print_r( sqlsrv_errors(), true));
-}
-
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo ($row['int_id_convenio'] . " - " . $row['str_nome_convenio'] . PHP_EOL);
-}
-sqlsrv_free_stmt($getResults);
-
-
-exit();
 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
