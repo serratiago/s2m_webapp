dea80cd7ba47c9ed55a33f4ee69b273639615293
serratiago <contato@tiagoserra.com>
2017-06-19 00:55:49 -0300

    banco
    teste

--- rest/index.php
+++ rest/index.php
@@ -18,12 +18,11 @@ $connectionOptions = array(
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 $tsql= "select int_id_convenio, str_nome_convenio from tbl_convenio";
 $getResults= sqlsrv_query($conn, $tsql);
-echo ("Reading data from table ..." . PHP_EOL);
-if ($getResults == FALSE)
-    echo (sqlsrv_errors());
+
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
     echo ($row['int_id_convenio'] . " " . $row['str_nome_convenio'] . PHP_EOL);
 }
+
 sqlsrv_free_stmt($getResults);
 
 
