e2222105367e5e80093ae64d23084c6ab49f07e0
serratiago <contato@tiagoserra.com>
2017-06-19 02:19:38 -0300

    Convenio
    lendo convenio

---
+++ rest/convenio/index.php
@@ -0,0 +1,34 @@
+<?php
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "dbservers2m@dbservers2m", "pwd" => "X28t12r80s", "Database" => "dbwebS2M", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dbservers2m.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+$tsql= "select int_id_convenio, str_nome_convenio from tbl_convenio";
+$getResults= sqlsrv_query($conn, $tsql);
+
+
+if( $conn === false ) {
+     die( print_r( sqlsrv_errors(), true));
+}
+
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    echo ($row['int_id_convenio'] . " - " . $row['str_nome_convenio'] . PHP_EOL);
+
+$int_id_convenio=$row['int_id_convenio']; 
+$str_nome_convenio=$row['str_nome_convenio']; 
+
+$posts[] = array('int_id_convenio'=> $int_id_convenio, 'str_nome_convenio'=> $str_nome_convenio);
+
+} 
+
+$response['posts'] = $posts;
+
+$fp = fopen('results.json', 'w');
+fwrite($fp, json_encode($response));
+fclose($fp);
+
+
+?> 
