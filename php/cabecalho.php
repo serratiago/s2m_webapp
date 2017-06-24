15726272843dab1366f9d70058ca12c055788db6
serratiago <contato@tiagoserra.com>
2017-06-24 03:10:13 -0300

    string
    valor errado

--- php/cabecalho.php
+++ php/cabecalho.php
@@ -13,13 +13,9 @@ include 'conexao.php';
     <title>Saúde Certa</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/padrao.css" rel="stylesheet">
-    
     <script type="text/javascript" src="/js/jquery-3.2.1.js"></script>
-   
  	<script type="text/javascript" src="/js/bootstrap.js"></script>
- 	
     <script type="text/javascript" src="/js/funcoes.js"></script>
     <!-- HTML5 shim e Respond.js para suporte no IE8 de elementos HTML5 e media queries -->
     <!-- ALERTA: Respond.js não funciona se você visualizar uma página file:// -->
