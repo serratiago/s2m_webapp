af2a93c3282541e7a373292cdece2412ea1fd4b0
serratiago <contato@tiagoserra.com>
2017-06-24 09:03:17 -0300

    grafico
    ambiente corpooral

--- paciente_leitura.php
+++ paciente_leitura.php
@@ -11,55 +11,7 @@ include("php/cabecalho.php");
   </div>
   <div class="panel-body">
   
- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-     
-           google.charts.load('current', {'packages':['line']});
-      google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Hora');
-      data.addColumn('number', 'Temperatura Ambiente');
-      data.addColumn('number', 'Temperatura Corporal');
-
-
-      data.addRows([
-        [1,  37.8, 80.8],
-        [2,  30.9, 69.5],
-        [3,  25.4,   57],
-        [4,  11.7, 18.8],
-        [5,  11.9, 17.6],
-        [6,   8.8, 13.6],
-        [7,   7.6, 12.3],
-        [8,  12.3, 29.2],
-        [9,  16.9, 42.9],
-        [10, 12.8, 30.9],
-        [11,  5.3,  7.9],
-        [12,  6.6,  8.4],
-        [13,  4.8,  6.3],
-        [14,  4.2,  6.2]
-      ]);
-
-      var options = {
-        chart: {
-          title: 'Box Office Earnings in First Two Weeks of Opening',
-          subtitle: 'in millions of dollars (USD)'
-        },
-        width: 900,
-        height: 500
-      };
-
-      var chart = new google.charts.Line(document.getElementById('linechart'));
-
-      chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-
-    </script>
-
-
-    <div id="linechart" style="width: 900px; height: 500px"></div>
+ 
 </div>
 </div>
 </div>
