1ba1077cbc13b148ce100aecff3cba67d6a58c28
serratiago <contato@tiagoserra.com>
2017-06-24 09:35:06 -0300

    erro
    string sql

--- php/grafico_temperatura_corporal_ambiente.php
+++ php/grafico_temperatura_corporal_ambiente.php
@@ -3,7 +3,7 @@
 include 'config.php';
 include 'conexao.php';
 
-$SQL = "SELECT SELECT int_id_msg, CONCAT(DATEPART(HOUR,dt_data_rec_msg),':', DATEPART(MINUTE,dt_data_rec_msg)) as hora";
+$SQL = "SELECT int_id_msg, CONCAT(DATEPART(HOUR,dt_data_rec_msg),':', DATEPART(MINUTE,dt_data_rec_msg)) as hora";
 $SQL .= " , pacienteId, temperaturaAmbiente, temperaturaCorporal FROM tbl_iot_monitor";
 $str_result = "";
 
