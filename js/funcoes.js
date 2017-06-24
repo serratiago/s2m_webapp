5b32fdc913d097049a63427f986eddac37c39fb6
serratiago <contato@tiagoserra.com>
2017-06-24 02:39:01 -0300

    tr
    tr clicavel

---
+++ js/funcoes.js
@@ -0,0 +1,14 @@
+$('.table tr.row-link').each(function(){
+    $(this).css('cursor','pointer').hover(
+        function(){ 
+            $(this).addClass('active'); 
+        },  
+        function(){ 
+            $(this).removeClass('active'); 
+        }).click( function(){ 
+            document.location = $(this).attr('data-href'); 
+        }
+    );
+});
+
+alert('oi');
