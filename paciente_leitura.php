5965a5c91d88a9049cec19bc04da1a6d4a9b01f6
serratiago <contato@tiagoserra.com>
2017-06-24 09:02:03 -0300

    grafico
    novo

--- paciente_leitura.php
+++ paciente_leitura.php
@@ -20,26 +20,26 @@ include("php/cabecalho.php");
     function drawChart() {
 
       var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Day');
-      data.addColumn('number', 'Guardians of the Galaxy');
-      data.addColumn('number', 'The Avengers');
-      data.addColumn('number', 'Transformers: Age of Extinction');
+      data.addColumn('number', 'Hora');
+      data.addColumn('number', 'Temperatura Ambiente');
+      data.addColumn('number', 'Temperatura Corporal');
+
 
       data.addRows([
-        [1,  37.8, 80.8, 41.8],
-        [2,  30.9, 69.5, 32.4],
-        [3,  25.4,   57, 25.7],
-        [4,  11.7, 18.8, 10.5],
-        [5,  11.9, 17.6, 10.4],
-        [6,   8.8, 13.6,  7.7],
-        [7,   7.6, 12.3,  9.6],
-        [8,  12.3, 29.2, 10.6],
-        [9,  16.9, 42.9, 14.8],
-        [10, 12.8, 30.9, 11.6],
-        [11,  5.3,  7.9,  4.7],
-        [12,  6.6,  8.4,  5.2],
-        [13,  4.8,  6.3,  3.6],
-        [14,  4.2,  6.2,  3.4]
+        [1,  37.8, 80.8],
+        [2,  30.9, 69.5],
+        [3,  25.4,   57],
+        [4,  11.7, 18.8],
+        [5,  11.9, 17.6],
+        [6,   8.8, 13.6],
+        [7,   7.6, 12.3],
+        [8,  12.3, 29.2],
+        [9,  16.9, 42.9],
+        [10, 12.8, 30.9],
+        [11,  5.3,  7.9],
+        [12,  6.6,  8.4],
+        [13,  4.8,  6.3],
+        [14,  4.2,  6.2]
       ]);
 
       var options = {
