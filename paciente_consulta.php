67b00301c9041306902d6b738f97d51fd26623c0
serratiago <contato@tiagoserra.com>
2017-06-24 03:11:52 -0300

    Atualizando
    Hellen

--- paciente_consulta.php
+++ paciente_consulta.php
@@ -11,7 +11,7 @@ include("php/cabecalho.php");
   </div>
   <div class="panel-body">
   
-<table id="click" class="table table-striped table-hover">
+<table class="table table-striped table-hover">
   <thead>
     <tr>
       <th>Registro</th>
@@ -46,7 +46,7 @@ $SQL .= " FROM tbl_paciente";
      $data = $dia."/".$mes."/".$ano;
 
 	 ?>
-	 <tr class="info row-link clickable" data-href="uol.com.br">
+	 <tr>
 	      <td><?php echo $rs['str_registro_paciente']; ?></td>
 	      <td><?php echo $rs['str_nome_paciente']; ?></td>
 	      <td><?php echo $sexo; ?></td>
