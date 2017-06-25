327a4c8a6d87a5e699a8b1cd6071353570705e57
serratiago <contato@tiagoserra.com>
2017-06-25 12:08:34 -0300

    load google
    teste

--- php/dados_reload.php
+++ php/dados_reload.php
@@ -35,8 +35,9 @@ $SQL .= "order by int_id_msg asc ";
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
      
-      google.charts.load('visualization', '1', {packages: ['line']});
-      google.charts.setOnLoadCallback(drawChart);
+
+    google.charts.load('visualization', '1', { 'callback': 'drawChart()', 'packages': ['line'] });
+ 
 
     function drawChart() {
 
