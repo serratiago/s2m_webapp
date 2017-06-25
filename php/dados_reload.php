f94b94337d69178fed66bf5d929b79825f971760
serratiago <contato@tiagoserra.com>
2017-06-25 08:12:21 -0300

    hora
    hora

--- php/dados_reload.php
+++ php/dados_reload.php
@@ -3,7 +3,7 @@ include 'config.php';
 include 'conexao.php';
 
 $SQL = "SELECT int_id_msg, hora, pacienteId, batimento, temperaturaAmbiente, temperaturaCorporal FROM ";
-$SQL .= "(SELECT top(100) int_id_msg, batimento, CONCAT(DATEPART(HOUR,HOUR,horaenvio),':', ";
+$SQL .= "(SELECT top(100) int_id_msg, batimento, CONCAT(DATEPART(HOUR,horaenvio),':', ";
 $SQL .= "DATEPART(MINUTE,horaenvio)) as hora, ";
 $SQL .= " pacienteId, temperaturaAmbiente, temperaturaCorporal FROM tbl_iot_monitor order by int_id_msg desc) tbl ";
 $SQL .= "order by int_id_msg asc ";
