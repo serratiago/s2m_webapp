cb701f25e76e51660db841df1e4d1b55daebb8ba
serratiago <contato@tiagoserra.com>
2017-06-25 00:11:46 -0300

    nova
    tela nova

---
+++ paciente_monitor.php
@@ -0,0 +1,71 @@
+<?php
+include("php/cabecalho.php");
+?>
+<div id="div_conteudo">
+
+   
+<div class="panel panel-info">
+
+ <div class="panel-heading">
+    <h3 class="panel-title">Pacientes</h3>
+  </div>
+  <div class="panel-body">
+  
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th>Registro</th>
+      <th>Nome</th>
+      <th>Sexo</th>
+      <th>Idade</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php
+
+$SQL = "SELECT int_id_paciente, str_nome_paciente, int_sexo_paciente, int_dt_nascimento_paciente, str_registro_paciente";
+$SQL .= " FROM tbl_paciente where int_id_paciente=1";
+
+
+	$query = $conn->prepare($SQL);
+    $query->execute();
+    $rs = $query->fetch()
+ 
+     
+     if( $rs['int_sexo_paciente'] == 1) {
+     	$sexo = "Masculino";
+     }else{
+		$sexo = "Feminino";
+     } 
+
+     $ano = substr($rs['int_dt_nascimento_paciente'],0,4);            
+     $mes = substr($rs['int_dt_nascimento_paciente'],4,2);  
+     $dia = substr($rs['int_dt_nascimento_paciente'],6,2);  
+     $data = $dia."/".$mes."/".$ano;
+
+	 ?>
+	 <tr class="info">
+	      <td><?php echo $rs['str_registro_paciente']; ?></td>
+	      <td><?php echo $rs['str_nome_paciente']; ?></td>
+	      <td><?php echo $sexo; ?></td>
+	      <td><?php echo $data; ?></td>
+	   </tr>
+
+  </tbody>
+</table>
+<div id="div_grafico_temperaturas">
+</div>
+
+</div>
+</div>
+</div>
+
+<script type="text/javascript">
+$.ajax({url: "demo_test.txt", success: function(result){
+        $("#div_grafico_temperaturas").html(result);
+    }});
+</script>
+<?
+include("php/rodape.php");
+?>
