66256bbbd005ff5e5ce6dddadff5203092991fc8
serratiago <contato@tiagoserra.com>
2017-06-25 08:38:57 -0300

    nome
    nome

--- php/cabecalho.php
+++ php/cabecalho.php
@@ -10,7 +10,7 @@ include 'conexao.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- As 3 meta tags acima *devem* vir em primeiro lugar dentro do `head`; qualquer outro conteúdo deve vir *após* essas tags -->
-    <title>Saúde Certa</title>
+    <title>Safe2Med</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
