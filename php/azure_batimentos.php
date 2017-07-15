c7e3cd79ef6cc9d823335ad14abd60218ba1ca72
serratiago <contato@tiagoserra.com>
2017-07-15 14:41:57 -0300

    azure
    azure

--- php/azure_batimentos.php
+++ php/azure_batimentos.php
@@ -1,1 +1,1 @@
-<iframe width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiMDU1MzdiNmEtNTkyNi00YmZmLWE2NTMtOTQzM2I1ZTUyNjJhIiwidCI6ImFhZTlhNWJjLWU2MjItNDQyYS1iNzFkLWUyMWVjMDE3NzkzMiJ9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe width="933" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiODczYWJhNjItNDY2My00MGY5LWE5MDQtZjU5NDk1ZjZlN2FkIiwidCI6ImFhZTlhNWJjLWU2MjItNDQyYS1iNzFkLWUyMWVjMDE3NzkzMiJ9" frameborder="0" allowFullScreen="true"></iframe>
