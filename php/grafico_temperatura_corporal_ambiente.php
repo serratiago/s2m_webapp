258505020945a348f3a535f80d2e8bb7d00c446e
serratiago <contato@tiagoserra.com>
2017-07-16 18:09:39 -0300

    teste
    teste

--- php/grafico_temperatura_corporal_ambiente.php
+++ php/grafico_temperatura_corporal_ambiente.php
@@ -54,7 +54,7 @@ new Morris.Line({
     stacked: true,
   	xLabelFormat: function(x) { return ''; },
   	xkey: 'y',
-  	ykeys: ['a', 'b'],
+  	ykeys: ['b'],
   	labels: ['Hora', 'Batimentos/Minuto'],
   	parseTime: false
 });
