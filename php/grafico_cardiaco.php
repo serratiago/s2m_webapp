a8973355efb8b3a16ce378493cb2cff35c180e98
serratiago <contato@tiagoserra.com>
2017-07-16 18:52:34 -0300

    order
    order

--- php/grafico_cardiaco.php
+++ php/grafico_cardiaco.php
@@ -13,10 +13,11 @@
 include 'config.php';
 include 'conexao.php';
 
-$SQL = "SELECT TOP (30) [int_id_log_monitor],[int_id_paciente_log_monitor],[dt_data_log_monitor]";
-$SQL .= ",[hr_data_log_monitor],[int_num_batimento_log_monitor],[int_num_temp_Corporal_log_monitor]";
-$SQL .= ",[int_num_temp_Ambiente_log_monitor],[int_num_umidade_Ambiente_log_monitor]";
-$SQL .= "FROM [dbo].[tbl_monitor] order by int_id_log_monitor desc";
+$SQL = "SELECT [int_id_log_monitor],[hr_data_log_monitor],[int_num_batimento_log_monitor],";
+$SQL .= "[int_num_temp_Corporal_log_monitor] FROM ";
+$SQL .= "(SELECT TOP (30) [int_id_log_monitor],[hr_data_log_monitor],[int_num_batimento_log_monitor],";
+$SQL .= "[int_num_temp_Corporal_log_monitor] ";
+$SQL .= "FROM [dbo].[tbl_monitor] order by [int_id_log_monitor] desc)";
 
 	$str_result = "";
 
