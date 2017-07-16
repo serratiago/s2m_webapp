c9b3b3219ced3e12e73f5ff6ee1113a9368d710d
serratiago <contato@tiagoserra.com>
2017-07-16 19:32:25 -0300

    chart
    chart

--- php/grafico_cardiaco.php
+++ php/grafico_cardiaco.php
@@ -41,7 +41,7 @@ $SQL .= "FROM [dbo].[tbl_monitor] order by [int_id_log_monitor] desc) tbl order 
 	}
 ?>
 
-<div id="div_chart" style="height: 250px; width: 450px"></div>
+<div id="div_chart" style="height: 250px; width: 100%"></div>
 
 </html>
 
